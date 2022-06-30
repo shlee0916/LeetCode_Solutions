9f8621890e94ddef25bb69b26d12c2a8dfbfc76d
GitHub-Joayo <dkssudxx@naver.com>
2022-06-30 23:27:32 +0900

    Feat: Update new solution 06/30
    First DFS problem.

---
+++ DFS/validate_binary_search_tree.py
@@ -0,0 +1,40 @@
+from tkinter.tix import Tree
+from typing import Optional
+
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+        
+        
+class Solution:
+    def isValidBST(self, root: Optional[TreeNode]) -> bool:
+        stack = []
+        prev = None
+        while root or stack:
+            while root:
+                stack.append(root)
+                root = root.left
+            root = stack.pop()
+            if prev and prev.val >= root.val:
+                return False
+            
+            prev = root
+            root = root.right
+            
+        return True
+    
+
+if __name__ == "__main__":
+    sol = Solution()
+    root1 = TreeNode(2, TreeNode(1), TreeNode(3))
+    root2 = TreeNode(5,
+                     TreeNode(1),
+                     TreeNode(4, TreeNode(3), TreeNode(6)))
+    
+    
+    print(sol.isValidBST(root1))
+    print(sol.isValidBST(root2))
