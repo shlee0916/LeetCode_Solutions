919001d2ad779e870f8e5f5bfff3c9215657abd3
GitHub-Joayo <shlee0916.dev@gmail.com>
2022-09-03 23:32:41 +0900

    Feat: Update new solution 09/03

---
+++ Easy/binary_serach.py
@@ -0,0 +1,30 @@
+'''
+
+'''
+from typing import List
+
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        begin = 0
+        end = len(nums)
+        
+        while begin < end:
+            mid = ((end - begin) // 2) + begin
+            
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < target:
+                begin = mid + 1
+            elif nums[mid] > target:
+                end = mid
+                
+        return -1
+    
+
+if __name__ == "__main__":
+    sol = Solution()
+    
+    print(sol.search([-1, 0, 3, 5, 9], 3))
+    print(sol.search([-1, 0, 3, 5, 9], -2))
+    print(sol.search([-1, 0, 3, 5, 9], 9))
