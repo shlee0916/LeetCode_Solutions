aa558d928db4bf7af46614fbd82ce7d2c64eebc9
shlee0916 <shlee0916.dev@gmail.com>
2023-04-03 15:20:33 +0900

    Feat: Update new solution 04/03

---
+++ Easy/fair_candy_swap.py
@@ -0,0 +1,29 @@
+'''
+https://leetcode.com/problems/fair-candy-swap/description/
+'''
+
+from typing import List
+
+
+class Solution:
+    def fairCandySwap(self, aliceSizes: List[int], bobSizes: List[int]) -> List[int]:
+        diff = (sum(aliceSizes) - sum(bobSizes)) / 2
+        alices = set(aliceSizes)
+        
+        for candy in bobSizes:
+            if diff + candy in alices:
+                return [diff + candy, candy]
+
+
+if __name__ == "__main__":
+    sol = Solution()
+
+    test1 = sol.fairCandySwap(aliceSizes = [1, 1], bobSizes = [2, 2])
+    assert test1 == [1, 2]
+
+    test2 = sol.fairCandySwap(aliceSizes = [1, 2], bobSizes = [2, 3])
+    assert test2 == [1, 2]
+
+    test3 = sol.fairCandySwap(aliceSizes = [2], bobSizes = [1, 3])
+    assert test3 == [2, 3]
+    
