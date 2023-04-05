9831f7598be74cc4c56fcd3b0f572f1d6f47b139
shlee0916 <shlee0916.dev@gmail.com>
2023-04-05 15:23:26 +0900

    Feat: Update new solution 04/05

---
+++ Medium/partition_array_according_to_given_pivot.py
@@ -0,0 +1,30 @@
+'''
+https://leetcode.com/problems/partition-array-according-to-given-pivot/description/
+'''
+
+from typing import List
+
+
+class Solution:
+    def pivotArray(self, nums: List[int], pivot: int) -> List[int]:
+        begin = 0
+        end = len(nums) - 1
+        result = [pivot] * (end + 1)
+        for idx, num in enumerate(nums):
+            if num < pivot:
+                result[begin], begin = num, begin + 1
+            if nums[-idx - 1] > pivot:
+                result[end], end = nums[-idx - 1], end - 1
+
+        return result
+
+
+if __name__ == "__mina__":
+    sol = Solution()
+
+    test1 = sol.pivotArray(nums = [9, 12, 5, 10, 14, 3, 10], pivot = 10)
+    assert test1 == [9, 5, 3, 10, 10, 12, 14]
+
+    test2 = sol.pivotArray(nums = [-3, 4, 3, 2], pivot = 2)
+    assert test2 == [-3, 2, 4, 3]
+    
